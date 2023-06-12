c9076be0f1b51ce2d9917c4f4babd13ad078f516
Stefan Caldararu <stefancaldararu@gmail.com>
2023-06-12 12:52:38 +0200

    changed to simplified 6DOF model for most forces and moments. Still dont have moments, but hippo_basic_sim seems to work now

--- hippo_basic_sim_GENMODEL.py
+++ hippo_basic_sim_GENMODEL.py
@@ -5,7 +5,6 @@
 #Ignoring Coriolis, and assuming values from
 import numpy as np 
 from quad_vis import vis
-from scipy.spatial.transform import Rotation
 
 M_A = np.diag([-1.11, -2.80, -2.80, -0.00451, -0.0163, -0.0163])
 I_g = np.diag([0.002408, 0.010717, 0.010717])
@@ -13,8 +12,59 @@ m = 1.43
 #FIXME: make sure we need the lower half of this matrix
 M_RB = np.diag([m, m, m, 0.002408, 0.010717, 0.010717])
 
+#total mass load
+M_T = M_RB+M_A
+M_Tinv = np.linalg.inv(M_T)
+
+def calcS(lamb):
+    l1 = lamb[0,0]
+    l2 = lamb[1,0]
+    l3 = lamb[2,0]
+    S = np.array([
+        [0,-l3, l2],
+        [l3, 0, -l1],
+        [-l2, l1, 0]
+    ])
+    return S
+
+def calcC_RB(nu):
+    o_b = np.array([[nu[3,0]],[nu[4,0]],[nu[5,0]]])
+    #S(o_b)
+    S1 = calcS(o_b)
+    #S(I_g@o_b)
+    S2 = calcS(I_g@o_b)
+    C_RB = np.zeros((6, 6), dtype=float)
+    C_RB[:3, :3] = m*S1
+    C_RB[3:, 3:] = S2
+    # C_RB = np.array([
+    #     [m*S1[0,0], m*S1[0,1], m*S1[0,2], 0,0,0],
+    #     [m*S1[1,0], m*S1[1,1], m*S1[1,2], 0,0,0],
+    #     [m*S1[2,0], m*S1[2,1], m*S1[2,2], 0,0,0],
+    #     [0,0,0,S2[2,0], S2[2,1], S2[2,2]],
+    #     [0,0,0,S2[2,0], S2[2,1], S2[2,2]],
+    #     [0,0,0,S2[2,0], S2[2,1],S2[2,2]]
+    # ])
+    return C_RB
+
+def calcC_A(nu):
+    v1 = np.array([[nu[0,0]], [nu[1,0]], [nu[2,0]]])
+    v2 = np.array([[nu[3,0]], [nu[4,0]], [nu[5,0]]])
+    A11 = M_A[:3, :3]
+    A12 = M_A[:3, -3:]
+    A21 = M_A[-3:, :3]
+    A22 = M_A[-3:, -3:]
+    #S(A11@v1+A12@v2)
+    S1 = calcS(A11@v1+A12@v2)
+    #S(A21@v1+A22@v2)
+    S2 = calcS(A21@v1+A22@v2)
+    C_A = np.zeros((6,6), dtype=float)
+    C_A[:3,3:] = -1*S1
+    C_A[3:,:3] = -1*S1
+    C_A[3:,3:] = -1*S2
+    return C_A
+
 #for now, assume Damping is diagonal constant...
-D_A = np.diag([-5.39, -17.36, -17.36, -0.00114, -0.007, -0.007])
+D_A = np.diag([5.39, 17.36, 17.36, -0.00114, -0.007, -0.007])
 # def calcD_A(nu):
 #     D_A = np.zeros((6,6), dtype = float)
 #     return D_A
@@ -26,25 +76,25 @@ def main():
     dt = 0.05
     eta = np.array([[0],[0],[3],[0.0],[0],[0]])
     nu = np.zeros((6,1), dtype = float)
-    #nu[0,0] = 10.1
+    #nu[3,0] = 0.1
 
     esc = 1650
-    ESC = np.array([[esc],[0],[esc],[0]])
+    ESC = np.array([[esc],[esc],[esc],[0]])
     start = 0
     end = 20
     time = np.arange(start, end, dt)
     plt = vis(0.5)#FIXME: make this so that it can vis horizontal
     
     for t in time:
-        if(t>1):
-            esc = 0
-            ESC = np.array([[esc],[esc],[esc],[esc]])
+        # if(t>0.1):
+        #     esc = 1650
+        #     ESC = np.array([[esc],[esc],[esc],[esc]])
 
         thrust = computeThrust(ESC)
-        print(thrust)
+        #print(thrust)
         eta, nu = motion_model(eta, nu, thrust, dt)
         plt.plot(eta[:3], eta[-3:])
-        print(eta)
+        # print(eta)
 
 
 
@@ -101,58 +151,17 @@ def computeThrust(ESC):
     
     return thrust
 def motion_model(eta, nu, thrust, dt):
-
-
-    #Split int two secitons, longitudinal dynamics (using u,w,q)
-
-    M_T = np.array([
-        [m-M_A[0,0], 0, 0],
-        [0, m-M_A[2,2], 0],
-        [0, 0, I_g[1,1]-M_A[4,4]]#TODO: make sure I_g correct here...
-    ])
-    D = np.diag([-D_A[0,0], -D_A[2,2], -D_A[4,4]])
-
-    C = np.array([
-        [0,0,0],
-        [0,0,-(m-M_A[0,0])*nu[0,0]],
-        [0, (M_A[2,2]-M_A[0,0])*nu[0,0], 0] #beacuse x_g = 0, 3,3 = 0
-    ])
-
-    #assuming hydrostatics is 0.
-    #thrust for this system: 
-    t1 = np.array([[thrust[0,0]], [thrust[2,0]], [thrust[4,0]]])
-    #state for this system: 
-    s1 = np.array([[nu[0,0]], [nu[2,0]], [nu[4,0]]])
-
-    RHS1 = t1-D@s1-C@s1
-    accel1 = np.linalg.inv(M_T)@RHS1
-    nu[0,0] = nu[0,0]+accel1[0,0]*dt
-    nu[2,0] = nu[2,0]+accel1[1,0]*dt
-    nu[4,0] = nu[4,0]+accel1[2,0]*dt
     
-    #Now we are doing lateral dynamics (v,p,r). For us, the linear dynamics of  v should be 0, but this is because the thrust in this dimension will be 0.
-
-    M_T = np.array([
-        [m-M_A[1,1], 0, 0],
-        [0, I_g[0,0]-M_A[3,3], 0],
-        [0, 0, I_g[2,2]-M_A[5,5]]#TODO: make sure I_g correct here...
-    ])
-    D = np.diag([-D_A[1,1], -D_A[3,3], -D_A[5,5]])
-    C = np.array([
-        [0,0,(m*M_A[0,0])*nu[0,0]],
-        [0,0,0],
-        [(M_A[0,0]-M_A[1,1])*nu[0,0], 0, 0]#because x_g = 0, 3,3 = 0
-    ])
-    t2 = np.array([[thrust[1,0]], [thrust[3,0]], [thrust[5,0]]])
-    #state for this system: 
-    s2 = np.array([[nu[1,0]], [nu[3,0]], [nu[5,0]]])
-    RHS2 = t2-D@s2-C@s2
-    accel2 = np.linalg.inv(M_T)@RHS2
-    nu[1,0] = nu[1,0]+accel2[0,0]*dt
-    nu[3,0] = nu[3,0]+accel2[1,0]*dt
-    nu[5,0] = nu[5,0]+accel2[2,0]*dt
-
-    #nu is now propogated. 
+    C_RB = calcC_RB(nu)
+    C_A = calcC_A(nu)
+    
+    #Right hand side of our equation...
+    #TODO: why do we need to add damping? this is not the way it should work...
+    RHS = thrust-D_A@nu-C_RB@nu-C_A@nu
+    nudot = M_Tinv@RHS
+    print(nudot)
+    nu = nu+dt*nudot
+    #nu was in the body reference frame. eta is with respect to the global reference frame. So, need to transfer both...
     body2world = np.zeros((6,6), dtype=float)
     theta = np.array([[eta[3,0]],[eta[4,0]],[eta[5,0]]])
 
@@ -160,6 +169,7 @@ def motion_model(eta, nu, thrust, dt):
     body2world[3:,3:] = body2worldTrans(theta)
     globalChange = body2world@nu
     eta = eta+dt*globalChange#FIXME: do we need to include nudot here too?
+
     return eta, nu
 #Rotation and transformation matrices...
 def body2worldTrans(Theta):
